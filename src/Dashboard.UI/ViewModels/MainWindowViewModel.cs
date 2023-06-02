5fb8ecf5fe221a10e279ccce59c949596090331b
Dominik.Kotecki <dominikus1910@gmail.com>
2023-06-02 17:48:14 +0200

    add avalonia

---
+++ src/Dashboard.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Dashboard.UI.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private string _greeting = "test";
+    
+    [RelayCommand]
+    public void ButtonClicked() => Greeting = "Hello, Avalonia! 3";
+}
